e64cff7611c836973463debe133f3a6750345637
yuannisa24si-beep <yuannisa24si@mahasiswa.pcr.ac.id>
2026-06-07 21:55:46 +0700

    hapus folder nested wedding-organizer

--- wedding-organizer/src/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children, maxWidth = 1200, padding = '24px' }) {
-  return (
-    <div style={{ maxWidth, margin: '0 auto', padding, width: '100%' }}>
-      {children}
-    </div>
-  )
-}
